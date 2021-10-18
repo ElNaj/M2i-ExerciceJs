904a35a33c0021d035f5bbdcb94547a36989ead1
ElNaj <nelfateoui@gmail.com>
2021-10-18 19:10:29 +0200

    Exo1 à Exo4 compris

---
+++ exo4.js
@@ -0,0 +1,11 @@
+// Programme qui demande à l'utilisateur de saisir 3 nombres puis trouver le plus grand
+
+let read = require("readline-sync");
+
+let saisiUser1 = read.questionInt("Saisir un premier nombre : " );
+let saisiUser2 = read.questionInt("Saisir un second nombre : " );
+let saisiUser3 = read.questionInt("Saisir un dernier nombre : " );
+
+let saisis = [saisiUser1, saisiUser2, saisiUser3]; 
+let valeurMax = Math.max(...saisis);
+console.log(valeurMax);
