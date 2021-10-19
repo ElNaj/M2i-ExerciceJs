9744cb26241cb63b19f09bf48ddf499896530d5e
ElNaj <nelfateoui@gmail.com>
2021-10-19 13:10:20 +0200

    Modif Type Exo8 à 10

---
+++ exo10.js
@@ -0,0 +1,19 @@
+// Programme demandant à l'utilisateur de saisir un nombre et 
+// génére une pyramide entière
+
+
+let read = require("readline-sync");
+let nombre = read.questionInt("Saisir un nombre : " );
+
+;
+
+for (let i = 0; i < nombre; i++) {
+    let etoile = "";
+    for (let j = 1; j < nombre - i; j++) {
+        etoile += " ";
+    }
+    for(let k = 1; k <= (2*i+1); k++) {
+        etoile += "*";
+    }
+    console.log(etoile);
+}
---
+++ exo8.js
@@ -0,0 +1,12 @@
+// Programme demandant à l'utilisateur de saisir un nombre et 
+// génére un escalier d'étoiles
+
+let read = require("readline-sync");
+let nombre = read.questionInt("Saisir un nombre : " );
+console.log(`Votre esacalier aura ${nombre} lignes`);
+
+let etiole = "";
+for (let i = 1; i <= nombre; i++) {
+        etiole += "*";
+        console.log(etiole);
+}
