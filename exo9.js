acf5fb4270b926cbe35b65add4b67c13686d541d
ElNaj <nelfateoui@gmail.com>
2021-10-19 12:26:48 +0200

    Exo9

--- exo9.js
+++ exo9.js
@@ -1,1 +1,17 @@
-// 
+// Programme demandant à l'utilisateur de saisir un nombre et 
+// génére une drôle de pyramide, adaptation exo8 pour faire descendre l'escalier
+
+let read = require("readline-sync");
+let nombre = read.questionFloat("Saisir un nombre : " );
+
+let etoile = "";
+
+
+for (let i = 1; i <= nombre; i++) {
+        etoile += "*";
+        console.log(etoile);
+}
+for (let j = nombre - 1; j >= 0; j--) {
+    etoile = "*".repeat(j);
+    console.log(etoile);
+}
